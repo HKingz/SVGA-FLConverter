8655883bbaf185a5d1792ad74d8f9892db3985e9
PonyCui <cuis@vip.qq.com>
2017-05-18 11:11:56 +0800

    解决引导层被引导元素缩放问题（旋转、拉伸除外）

--- src/js/modules/SVGAPathHelper.js
+++ src/js/modules/SVGAPathHelper.js
@@ -6,23 +6,27 @@ module.exports = class SVGAPathHelper {
         this.graphics = graphics;
     }
 
-    requestPath = (offset = {x: 0.0, y: 0.0}) => {
+    requestPoint = (value = 0.0, offset = 0.0, scale = 1.0, reg = 0.0) => {
+        return (((value + offset) - reg) * scale + reg).toFixed(6)
+    }
+
+    requestPath = (offset = {x: 0.0, y: 0.0}, scale = {x: 1.0, y: 1.0}, reg = {x: 0.0, y: 0.0}) => {
         let instructions = this.graphics.getInstructions();
         let d = "";
         for (let index = 0; index < instructions.length; index++) {
             let element = instructions[index];
             if (element instanceof createjs.Graphics.BeginPath) {}
             else if (element instanceof createjs.Graphics.MoveTo) {
-                d += "M " + (element.x + offset.x).toFixed(3) + " " + (element.y + offset.y).toFixed(3) + " ";
+                d += "M " + this.requestPoint(element.x, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.y, offset.y, scale.y, reg.y) + " ";
             }
             else if (element instanceof createjs.Graphics.LineTo) {
-                d += "L " + (element.x + offset.x).toFixed(3) + " " + (element.y + offset.y).toFixed(3) + " ";
+                d += "L " + this.requestPoint(element.x, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.y, offset.y, scale.y, reg.y) + " ";
             }
             else if (element instanceof createjs.Graphics.BezierCurveTo) {
-                d += "C " + (element.cp1x + offset.x).toFixed(3) + " " + (element.cp1y + offset.y).toFixed(3) + " " + (element.cp2x + offset.x).toFixed(3) + " " + (element.cp2y + offset.y).toFixed(3) + " " + (element.x + offset.x).toFixed(3) + " " + (element.y + offset.y).toFixed(3) + " ";
+                d += "C " + this.requestPoint(element.cp1x, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.cp1y, offset.y, scale.y, reg.y) + " " + this.requestPoint(element.cp2x, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.cp2y, offset.y, scale.y, reg.y) + " " + this.requestPoint(element.x, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.y, offset.y, scale.y, reg.y) + " ";
             }
             else if (element instanceof createjs.Graphics.QuadraticCurveTo) {
-                d += "Q " + (element.cpx + offset.x).toFixed(3) + " " + (element.cpy + offset.y).toFixed(3) + " " + (element.x + offset.x).toFixed(3) + " " + (element.y + offset.y).toFixed(3) + " ";
+                d += "Q " + this.requestPoint(element.cpx, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.cpy, offset.y, scale.y, reg.y) + " " + this.requestPoint(element.x, offset.x, scale.x, reg.x) + " " + this.requestPoint(element.y, offset.y, scale.y, reg.y) + " ";
             }
             else if (element instanceof createjs.Graphics.ClosePath) {
                 d += "Z ";
